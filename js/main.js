7c416582edf8b1e3bc465b29885ce54ccad8ee42
amare1990 <amaremek@gmail.com>
2022-10-01 16:08:19 +0300

    Fix JS linters errors

--- js/main.js
+++ js/main.js
@@ -1,38 +1,33 @@
 window.onload = () => {
-  //const booksContainer = document.querySelector('.container-book');
-
   const addBtn = document.querySelector('.add-btn');
-  //const parser = new DOMParser();
-
   class Book {
     constructor(title, author) {
       this.title = title;
       this.author = author;
     }
   }
- 
+
   class BookImplement {
-      constructor () {
-        this.bookArray = [];
-        this.booksContainer = document.querySelector('.book-from-storage');
-        this.parser = new DOMParser();
+    constructor() {
+      this.bookArray = [];
+      this.booksContainer = document.querySelector('.book-from-storage');
+      this.parser = new DOMParser();
+    }
+
+    removeBook(e, newBookElement) {
+      const index = e.target.getAttribute('myIndex');
+
+      function checkBtnclicked(element, i) {
+        if (i === parseInt(index, 10)) {
+          return false;
+        }
+        return true;
       }
 
-      removeBook(e, newBookElement) {
-        const index = e.target.getAttribute('myIndex');
-
-        function checkBtnclicked(element, i) {
-          if (i === parseInt(index, 10)) {
-            return false;
-          }
-          return true;
-          }
-
-        this.bookArray = this.bookArray.filter(checkBtnclicked);
-        newBookElement.remove();
-        //syncStorage();
-        localStorage.setItem('bookKey2', JSON.stringify(bookImplemObj.bookArray));
-        this.showBooks();
+      this.bookArray = this.bookArray.filter(checkBtnclicked);
+      newBookElement.remove();
+      localStorage.setItem('bookKey2', JSON.stringify(bookImplemObj.bookArray));
+      this.showBooks();
     }
 
     showBooks() {
@@ -44,30 +39,23 @@ window.onload = () => {
             <button type="button" class="remove-btn" myIndex ="${i}" >Remove</button>
           </div>
         `;
-        const newBookElement = this.parser.parseFromString(newBook, 'text/html').body.firstChild;
-        console.log(newBookElement);
-        const removeBtn = newBookElement.querySelector('.remove-btn');
+        const newBookElement = this.parser.parseFromString(newBook, 'text/html').body.firstChild; const removeBtn = newBookElement.querySelector('.remove-btn');
         removeBtn.addEventListener('click', (e) => {
           this.removeBook(e, newBookElement);
-          //this.syncStorage();
         });
         this.booksContainer.append(newBookElement);
       });
     }
-
-  } //End of BookImplement class
+  } // End of BookImplement class
 
   const bookImplemObj = new BookImplement();
 
   const bookStorage = localStorage.getItem('bookKey2');
 
   if (bookStorage) {
-    //method.bookList = JSON.parse(bookStorage);
     bookImplemObj.bookArray = JSON.parse(bookStorage);
     bookImplemObj.showBooks();
   }
- 
-  //bookImplemObj.showBooks();
 
   addBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -76,14 +64,8 @@ window.onload = () => {
     const book = new Book(title, author);
     bookImplemObj.bookArray.push(book);
     bookImplemObj.showBooks();
-    //syncStorage();
     localStorage.setItem('bookKey2', JSON.stringify(bookImplemObj.bookArray));
     document.querySelector('.title').value = '';
     document.querySelector('.author').value = '';
   });
-
-  //localStorage.removeItem('bookKey2');
-
 }; // End of Window onload event
-
-//localStorage.removeItem('bookKey2');
